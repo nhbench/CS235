1b164d3813ef6e5641b9ecdcf665b70958099376
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-04-27 23:25:38 -0700

    week 3
    week 3

---
+++ week03/week03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  week03
+//
+//  Created by Jeffry Simpson on 4/27/17.
+//  Copyright © 2017 Jeffry Simpson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
