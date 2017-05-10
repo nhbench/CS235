4df0d701353a2f438bee74de0c3f22a03bf4dc01
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-05-09 21:47:33 -0700

    pseudo code for now showing function.

--- week04/week04/nowServing.cpp
+++ week04/week04/nowServing.cpp
@@ -31,8 +31,85 @@ void nowServing()
 
    // your code here
 
+   /*
+    int Linecount = 0, minLeft;
+    
+    enum choices
+    !!
+    none
+    finished
+    Max_out
+    
+    class  Lab
+    string course
+    string name
+    int min
+    bool emg
+    
+    deque <Lab> Peoples
+    Lab current, next
+    
+    do(
+    
+    switch
+    {
+    
+    case: !!
+    if we are not serving anyone (minleft < 0)
+    next.emg = true
+    then current = next
+    minLeft = next.min
+    
+    else
+    Next. push to front()
+    
+    
+    case: none
+    if( minleft <= 0)
+    then check deque and grab next current.front()
+    
+    case: Finish
+    quit(0)
+    
+    default: case
+    make its structured right  XXXX(class)   and XXXXX(name) and either NN minutes or 1 default
+    next.course, student, and min
+    
+    if we are not serving anyone i=MinLeft <= 0
+    current = next
+    minLeft = min
+    else
+    next -> push to back()
+    
+    
+    }
+    
+    
+    
+    //display
+    if minLeft > 0
+    {
+    if current.emg == True
+    emergancy << current.student
+    else 
+    current.course << current.student << "time left: << i
+    }
+    else // min == 0	
+    //do nothing
+    
+    cout << <linecount++ >
+    if(minLeft > 0
+    minLeft--
+    
+    
+    while( xxx != "finished)
+    
+    
+    */
+   
    // end
    cout << "End of simulation\n";
+   
 }
 
 
