f5123dd5d800641fa4aafb0051bff576ea885ee5
David Perez <3dgar@gmail.com>
2017-05-11 16:54:18 -0700

    Finish deque implementation. Resize works

--- week04/week04/deque.h
+++ week04/week04/deque.h
@@ -2,7 +2,7 @@
 * Header:
 *    deque.h
 * Summary:
-*    Create a class encapsulating the notion of a deque
+*    A double ended queue.
 * Author
 *    Jeffry Simpson, David Perez
 ************************************************************************///
@@ -22,7 +22,7 @@ template <class T>
 class Deque
 {
 private:
-   // Utility
+   // Deque circular array
    int iAbsoluteFromIRelative(int i) const
    {
       return (i >= 0) ? (i % max) : (max - ((-1 - i) % max) - 1);
@@ -42,8 +42,7 @@ public:
 
    //Operator =
    Deque <T> & operator = (const Deque <T> & rightHandSide) throw(const char *);
-   //Display
-   void display(ostream & out) const;
+
    //Inline functions
    bool empty()   const { return size() == 0; }
    int size()     const { return iBack - iFront + 1; }
@@ -67,16 +66,17 @@ public:
 * Deque :: Non-Default Constructor
 *******************************************/
 template <class T>
-Deque  <T> ::Deque(int capacity) throw(const char *)
+Deque  <T>::Deque(int capacity) throw(const char *)
 {
+   assert(capacity > 0);
    // Set custom capacity
    max = capacity;
    data = new T[capacity];
 
+   // If allocated, start default values
    if (data != NULL)
    {
-      iFront = 0;
-      iBack = -1;
+      clear();
    }
 }
 
@@ -84,14 +84,16 @@ Deque  <T> ::Deque(int capacity) throw(const char *)
 * Deque :: Copy Constructor
 *******************************************/
 template <class T>
-Deque <T> ::Deque(const Deque & original) throw(const char *)
+Deque <T>::Deque(const Deque & original) throw(const char *)
 {
+   // Default front and back. Allocate
    clear();
    if (data == NULL)
       max = 2;
    max = original.max;
    data = new T[max];
-   
+
+   // Copy
    int j = 0;
    for (int i = original.iFront; i <= original.iBack; i++)
    {
@@ -104,16 +106,18 @@ Deque <T> ::Deque(const Deque & original) throw(const char *)
 * Deque :: Operator =
 *******************************************/
 template <class T>
-Deque <T> & Deque <T> ::operator=(const Deque <T> & rightHandSide) throw(const char *)
+Deque <T> & Deque <T>::operator=(const Deque <T> & rightHandSide) throw(const char *)
 {
    if (this != &rightHandSide)                // check that not st = st
    {
+      // Default front and back. Allocate
       clear();
       if (data == NULL)
          max = 2;
       max = rightHandSide.max;
       data = new T[max];
 
+      // Copy
       int j = 0;
       for (int i = rightHandSide.iFront; i <= rightHandSide.iBack; i++)
       {
@@ -125,19 +129,8 @@ Deque <T> & Deque <T> ::operator=(const Deque <T> & rightHandSide) throw(const c
 }
 
 /*******************************************
-* Deque :: Display()
-*          Show array contents
-*******************************************/
-template <class T>
-void Deque <T> ::display(ostream & out) const
-{
-   // Display, used for debugging
-   
-}
-
-/*******************************************
 * Deque :: push_back()
-*          Add content to end of array
+*          Add content to back of deque
 *******************************************/
 template <class T>
 void Deque <T>::push_back(const T & t)   throw(const char *)
@@ -149,45 +142,48 @@ void Deque <T>::push_back(const T & t)   throw(const char *)
       data = new T[max];
    }
 
+   // Reallocate if full
    if (size() == capacity())
       resize(max);
 
+   // Push to back
    iBack++;
    data[getIBack()] = t;
 }
 
 /*******************************************
 * Deque :: push_front()
-*          Add content to end of array
+*          Add content to front of deque
 *******************************************/
 template <class T>
 void Deque<T>::push_front(const T & t)   throw(const char *)
 {
    // NULL CHECK
-   if(data == NULL)
+   if (data == NULL)
    {
       max = 2;
       data = new T[max];
    }
 
+   // Reallocate if full
    if (size() == capacity())
       resize(max);
 
+   // Push to front
    iFront--;
    data[getIFront()] = t;
-
 }
 
 /*******************************************
 * Deque :: pop_back()
-*          Remove/advance head by one
+*          Remove from back
 *******************************************/
 template <class T>
 void Deque<T>::pop_back()               throw(const char *)
 {
    if (empty())
    {
-      throw "ERROR";
+      throw "ERROR: unable to pop from the back of empty deque";
    }
    else
       iBack--;
@@ -195,23 +191,22 @@ void Deque<T>::pop_back()               throw(const char *)
 
 /*******************************************
 * Deque :: pop_front()
-*          Remove/advance head by one
+*          Remove from front
 *******************************************/
 template <class T>
 void Deque<T>::pop_front()               throw(const char *)
 {
    if (empty())
    {
-      throw "Error";
+      throw "ERROR: unable to pop from the front of empty deque";
    }
    else
       iFront++;
 }
 
-
 /*******************************************
 * Deque :: front()
-*          Return the head by reference
+*          Return the front by reference
 *******************************************/
 template <class T>
 T & Deque<T>::front() throw(const char *)
@@ -220,13 +215,13 @@ T & Deque<T>::front() throw(const char *)
       return data[getIFront()];
    else
    {
-      throw "Nothing";
+      throw "ERROR: unable to access data from an empty deque";
    }
 }
 
 /*******************************************
 * Deque :: front()
-*          Return the head as copy
+*          Return the front as copy
 *******************************************/
 template <class T>
 T Deque<T>::front()   const throw(const char *)
@@ -235,13 +230,13 @@ T Deque<T>::front()   const throw(const char *)
       return data[getIFront()];
    else
    {
-      throw "nothing";
+      throw "ERROR: unable to access data from an empty deque";
    }
 }
 
 /*******************************************
 * Deque :: back()
-*          Return the tail by reference
+*          Return the back by reference
 *******************************************/
 template <class T>
 T & Deque<T>::back() throw(const char *)
@@ -250,13 +245,13 @@ T & Deque<T>::back() throw(const char *)
       return data[getIBack()];
    else
    {
-      throw "nothing";
+      throw "ERROR: unable to access data from an empty deque";
    }
 }
 
 /*******************************************
 * Deque :: back()
-*          Return the tail by copy
+*          Return the back by copy
 *******************************************/
 template <class T>
 T Deque<T>::back()   const throw(const char *)
@@ -265,98 +260,39 @@ T Deque<T>::back()   const throw(const char *)
       return data[getIBack()];
    else
    {
-      return "nothing";
+      return "ERROR: unable to access data from an empty deque";
    }
 }
 
 /***************************************************
-* Deques :: resize()
-* Used for changing the size of oldBuffer
+* Deque :: resize()
+* Used for doubling the size of data
 **************************************************/
 template <class T>
 void Deque<T>::resize(int & newValue) throw (const char *)
 {
-   const int oldCap = newValue;
+   assert(newValue > 0);
+   int oldMax = newValue;
+   int oldIFront = getIFront();
 
-   // Old values
-   int oldBack = iBack;
-   int oldFront = iFront;
+   // Double size
+   newValue *= 2;
+   T* newData = new T[newValue];
 
-   T* oldData = new T[oldCap];
+   // Reset head and tail
+   clear();
 
-   
-   if (getIBack() < getIFront())
+   // Loop through old array into new one
+   for (int i = 0; i < oldMax; i++)
    {
-      int back = 0;
-      int front = getIFront();
-      // Copy back
-      for (back; back < getIFront(); back++)
-      {
-         oldData[back] = data[back];
-      }
-      
-      // Copy front
-      for (front; front < oldCap; front++)
-      {
-         oldData[front] = data[front];
-        
-      }
-
-      // Reset settings and double
-      iFront = 0;
-      iBack = -1;
-      newValue *= 2;
-      T* newData = new T[newValue];
-
-      // Copy back
-      for (int i = 0; i < back; i++)
-      {
-         iBack++;
-         newData[getIBack()] = oldData[i];
-      }
-
-      // Copy front
-      for (int i = front; i >= back; i--)
-      {
-         iFront--;
-         newData[getIFront() + 1] = oldData[i];
-      }
-
-      // Needs this for some reason...
-      iFront++;
-
-      delete[] data;
-      delete[] oldData;
-      data = newData;
+      int index = (i + oldIFront) % oldMax;
+      iBack++;
+      newData[getIBack()] = data[index];
    }
-   else
-   {
-      int back = getIBack();
-      int front = 0;
-      
-      // Copy back
-      for (int i = 0; i <= getIBack(); i++)
-         oldData[i] = data[i];
 
-      // Reset settings
-      iFront = 0;
-      iBack = -1;
-      newValue *= 2;
-      T* newData = new T[newValue];
-
-      // copy
-      for (int i = 0; i <= back; i++)
-      {
-         iBack++;
-         newData[getIBack()] = oldData[i];
-      }
-
-      delete[] data;
-      delete[] oldData;
-      data = newData;
-   }
-   
+   // Cleanup
+   delete[] data;
+   data = newData;
 }
-
 
 #endif /* Deque_h */
