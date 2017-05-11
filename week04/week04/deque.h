b46fc200bd613d7c9c35cef8c19a14a503991bd5
David Perez <3dgar@gmail.com>
2017-05-11 14:45:42 -0700

    Deque prototype, fixing reallocation

--- week04/week04/deque.h
+++ week04/week04/deque.h
@@ -4,7 +4,7 @@
 * Summary:
 *    Create a class encapsulating the notion of a deque
 * Author
-*    Jeffry Simpson, David Perez 
+*    Jeffry Simpson, David Perez
 ************************************************************************///
 
 #ifndef deque_h
@@ -22,83 +22,81 @@ template <class T>
 class Deque
 {
 private:
-   int m_Capacity, numPop, numPush;
-   T * m_Array;
-
+   // Utility
+   int iAbsoluteFromIRelative(int i) const
+   {
+      return (i >= 0) ? (i % max) : (max - ((-1 - i) % max) - 1);
+   }
+   int getIBack()    const { return iAbsoluteFromIRelative(iBack); }
+   int getIFront()   const { return iAbsoluteFromIRelative(iFront); }
+   void resize(int & newValue) throw(const char *);
+   // Member variables
+   T * data;
+   int max, iFront, iBack;
 public:
    //Constructors
-   Deque() : m_Capacity(0), numPop(0), numPush(0), m_Array(NULL) {}   //Default
-   Deque(int numElements) throw(const char *);            //Non-Default
-   Deque(const Deque & original) throw(const char *);     //Copy
-
-                                                          //Destructur
-   ~Deque() { delete[] m_Array; }
+   Deque() : data(NULL), max(0), iFront(0), iBack(-1) {}
+   Deque(int capacity)           throw (const char *);
+   Deque(const Deque<T> & rhs)   throw (const char *);
+   ~Deque() { if (!empty()) delete[] data; }
 
    //Operator =
    Deque <T> & operator = (const Deque <T> & rightHandSide) throw(const char *);
-
-   //Inline functions
-   bool  empty()      const { return (size() == 0); }
-   int   size()       const { return numPush - numPop; }
-   int   capacity()   const { return m_Capacity; }
-   void  clear() { numPop = numPush = 0; }
-   int iTail()        const { return (numPush - 1) % m_Capacity; }
-   int iHead()        const { return numPop % m_Capacity; }
-
-   //Functions prototypes
+   //Display
    void display(ostream & out) const;
+   //Inline functions
+   bool empty()   const { return size() == 0; }
+   int size()     const { return iBack - iFront + 1; }
+   int capacity() const { return max; }
+   void clear() { iFront = 0, iBack = -1; }
 
-   void push(const T & newValue)  throw (const char *);
-   void pop()                     throw (const char *);
-   T & front()                    throw (const char *);
-   T   front()              const throw (const char *);
-   void front(int newValue) const throw (const char *);
-   T back() const throw(const char *);
+   // Unique deque functions
+   void push_back(const T & t)   throw(const char *);
+   void push_front(const T & t)  throw(const char *);
+   void pop_back()               throw(const char *);
+   void pop_front()              throw(const char *);
 
-   void reallocate(const int &newSize = 0) throw (const char *);
+   // Front & back Getters
+   T & front() throw(const char *);
+   T & back()  throw(const char *);
+   T front()   const throw(const char *);
+   T back()    const throw(const char *);
 };
 
 /*******************************************
 * Deque :: Non-Default Constructor
 *******************************************/
 template <class T>
-Deque  <T> ::Deque(int numElements) throw(const char *)
+Deque  <T> ::Deque(int capacity) throw(const char *)
 {
-   assert(numElements  > 0);
+   // Set custom capacity
+   max = capacity;
+   data = new T[capacity];
 
-   // Allocate array
-   m_Capacity = numElements;
-   m_Array = new(nothrow) T[m_Capacity];
-
-   // If allocated, set member values to default
-   if (m_Array != NULL)
+   if (data != NULL)
    {
-      numPop = 0;
-      numPush = 0;
+      iFront = 0;
+      iBack = -1;
    }
-   else
-      throw "Error: No memory to allocate";
 }
 
 /*******************************************
 * Deque :: Copy Constructor
 *******************************************/
 template <class T>
-Deque <T> ::Deque(const Deque & original) throw(const char *) : m_Capacity(original.m_Capacity), numPop(0), numPush(0)
+Deque <T> ::Deque(const Deque & original) throw(const char *)
 {
-   // No memory, lets make some!
-   if (m_Capacity == 0 || m_Array == NULL)
-   {
-      m_Capacity = 2;
-   }
-   // Allocate memory
-   m_Array = new T[m_Capacity];
-
+   clear();
+   if (data == NULL)
+      max = 2;
+   max = original.max;
+   data = new T[max];
+   
    int j = 0;
-   for (int i = original.numPop; i < original.numPush; i++)
+   for (int i = original.iFront; i <= original.iBack; i++)
    {
-      m_Array[j++] = original.m_Array[i % original.capacity()];
-      numPush++;
+      iBack++;
+      data[j++] = original.data[iAbsoluteFromIRelative(i)];
    }
 }
 
@@ -110,21 +108,17 @@ Deque <T> & Deque <T> ::operator=(const Deque <T> & rightHandSide) throw(const c
 {
    if (this != &rightHandSide)                // check that not st = st
    {
-      // Copy all member variables
-      this->m_Capacity = rightHandSide.capacity();
-      this->numPop = rightHandSide.numPop;
-      this->numPush = rightHandSide.numPush;
-      m_Array = new T[m_Capacity];
+      clear();
+      if (data == NULL)
+         max = 2;
+      max = rightHandSide.max;
+      data = new T[max];
 
-      // Copy contents to new array
-      int i, j;
-      for (i = this->numPop, j = 0; j < this->size(); i++, j++)
+      int j = 0;
+      for (int i = rightHandSide.iFront; i <= rightHandSide.iBack; i++)
       {
-         if (i == m_Capacity)
-         {
-            i = 0;
-         }
-         m_Array[i] = rightHandSide.m_Array[i];
+         data[j++] = rightHandSide.data[iAbsoluteFromIRelative(i)];
+         iBack++;
       }
    }
    return *this;
@@ -138,73 +132,95 @@ template <class T>
 void Deque <T> ::display(ostream & out) const
 {
    // Display, used for debugging
-   out << "Head: " << m_Array[iHead()] << endl;
-   out << "Tail: " << m_Array[iTail()] << endl;
-   out << "Capacity: " << m_Capacity << endl;
+   
 }
 
 /*******************************************
-* Deque :: push()
+* Deque :: push_back()
 *          Add content to end of array
 *******************************************/
 template <class T>
-void Deque <T> ::push(const T & newValue) throw(const char *)
+void Deque <T>::push_back(const T & t)   throw(const char *)
 {
-   // Make sure memory is allocated
-   if (m_Array == NULL)
+   // NULL CHECK
+   if (data == NULL)
    {
-      m_Capacity = 2;
-      try
-      {
-         m_Array = new T[m_Capacity];
-      }
-      catch (bad_alloc)
-      {
-         throw "ERROR: Can't allocate memory for the Deque!";
-      }
+      max = 2;
+      data = new T[max];
    }
 
-   // Capacity full, double size
    if (size() == capacity())
-   {
-      m_Capacity *= 2;
-      reallocate(m_Capacity);
-   }
+      resize(max);
 
-   // Push to new tail
-   numPush++;
-   m_Array[iTail()] = newValue;
+   iBack++;
+   data[getIBack()] = t;
 }
 
 /*******************************************
-* Deque :: pop()
+* Deque :: push_front()
+*          Add content to end of array
+*******************************************/
+template <class T>
+void Deque<T>::push_front(const T & t)   throw(const char *)
+{
+   // NULL CHECK
+   if(data == NULL)
+   {
+      max = 2;
+      data = new T[max];
+   }
+
+   if (size() == capacity())
+      resize(max);
+
+   iFront--;
+   data[getIFront()] = t;
+
+}
+
+/*******************************************
+* Deque :: pop_back()
 *          Remove/advance head by one
 *******************************************/
 template <class T>
-void Deque <T> ::pop() throw(const char *)
+void Deque<T>::pop_back()               throw(const char *)
 {
-   if (!empty())
-      numPop++;
-   else
+   if (empty())
    {
-      throw "ERROR: attempting to pop from an empty deque";
+      throw "ERROR";
    }
+   else
+      iBack--;
 }
+
+/*******************************************
+* Deque :: pop_front()
+*          Remove/advance head by one
+*******************************************/
+template <class T>
+void Deque<T>::pop_front()               throw(const char *)
+{
+   if (empty())
+   {
+      throw "Error";
+   }
+   else
+      iFront++;
+}
+
 
 /*******************************************
 * Deque :: front()
 *          Return the head by reference
 *******************************************/
 template <class T>
-T & Deque <T> ::front()       throw (const char *)
+T & Deque<T>::front() throw(const char *)
 {
    if (!empty())
-      return m_Array[iHead()];      // *X* return m_Array[m_Front];
+      return data[getIFront()];
    else
    {
-      throw "ERROR: attempting to access an item in an empty deque";
-      T garbage;
-      return garbage;
+      throw "Nothing";
    }
 }
 
@@ -213,72 +229,134 @@ T & Deque <T> ::front()       throw (const char *)
 *          Return the head as copy
 *******************************************/
 template <class T>
-T Deque <T> ::front() const throw (const char *)
+T Deque<T>::front()   const throw(const char *)
 {
    if (!empty())
-      return m_Array[iHead()];      // *X* return m_Array[m_Front];
+      return data[getIFront()];
    else
    {
-      throw "ERROR: attempting to access an item in an empty deque";
-      T garbage;
-      return garbage;
-   }
-}
-
-/*******************************************
-* Deque :: front()
-*          Set the front with new value
-*******************************************/
-template <class T>
-void Deque <T> ::front(int newValue) const throw (const char *)
-{
-   if (!empty())
-      m_Array[iHead()] = newValue;    //*X*   m_Array[m_Front] = newValue;
-   else
-   {
-      throw "ERROR: attempting to access an item in an empty deque";
+      throw "nothing";
    }
 }
 
 /*******************************************
 * Deque :: back()
-*          Return the tail
+*          Return the tail by reference
 *******************************************/
 template <class T>
-T Deque <T> ::back() const throw (const char *)
+T & Deque<T>::back() throw(const char *)
 {
    if (!empty())
-      return m_Array[iTail()];     // *X* return m_Array[m_Back];
+      return data[getIBack()];
    else
    {
-      throw  "ERROR: attempting to access an item in an empty deque";
+      throw "nothing";
+   }
+}
+
+/*******************************************
+* Deque :: back()
+*          Return the tail by copy
+*******************************************/
+template <class T>
+T Deque<T>::back()   const throw(const char *)
+{
+   if (!empty())
+      return data[getIBack()];
+   else
+   {
+      return "nothing";
    }
 }
 
 /***************************************************
-* Deques :: reallocate()
+* Deques :: resize()
 * Used for changing the size of oldBuffer
 **************************************************/
 template <class T>
-void Deque <T>::reallocate(const int &newSize) throw (const char *)
+void Deque<T>::resize(int & newValue) throw (const char *)
 {
-   // double the size
-   int oldSize = newSize / 2;
+   const int oldCap = newValue;
 
-   // Allocate new array
-   T* newArray = new T[newSize];
+   // Old values
+   int oldBack = iBack;
+   int oldFront = iFront;
 
-   // Copy with wrap
-   for (int i = 0; i < size(); i++)
-      newArray[i] = m_Array[(iHead() + i) % oldSize];
+   T* oldData = new T[oldCap];
 
-   // Cleanup, set new pointer
-   delete[] m_Array;
-   m_Array = newArray;
+   
+   if (getIBack() < getIFront())
+   {
+      int back = 0;
+      int front = getIFront();
+      // Copy back
+      for (back; back < getIFront(); back++)
+      {
+         oldData[back] = data[back];
+      }
+      
+      // Copy front
+      for (front; front < oldCap; front++)
+      {
+         oldData[front] = data[front];
+        
+      }
 
-   numPush = size();       //reset push
-   numPop = 0;             //reset pop
+      // Reset settings and double
+      iFront = 0;
+      iBack = -1;
+      newValue *= 2;
+      T* newData = new T[newValue];
 
+      // Copy back
+      for (int i = 0; i < back; i++)
+      {
+         iBack++;
+         newData[getIBack()] = oldData[i];
+      }
+
+      // Copy front
+      for (int i = front; i >= back; i--)
+      {
+         iFront--;
+         newData[getIFront() + 1] = oldData[i];
+      }
+
+      // Needs this for some reason...
+      iFront++;
+
+      delete[] data;
+      delete[] oldData;
+      data = newData;
+   }
+   else
+   {
+      int back = getIBack();
+      int front = 0;
+      
+      // Copy back
+      for (int i = 0; i <= getIBack(); i++)
+         oldData[i] = data[i];
+
+      // Reset settings
+      iFront = 0;
+      iBack = -1;
+      newValue *= 2;
+      T* newData = new T[newValue];
+
+      // copy
+      for (int i = 0; i <= back; i++)
+      {
+         iBack++;
+         newData[getIBack()] = oldData[i];
+      }
+
+      delete[] data;
+      delete[] oldData;
+      data = newData;
+   }
+   
 }
+
 
 #endif /* Deque_h */
